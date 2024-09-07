1af0c04f93ac81d052279ac2ccbc26679aa76f01
David Romero <48596887+dl-romero@users.noreply.github.com>
2024-09-07 09:26:11 -0700

    Update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='itadvisor_client',
+version='0.1.0',
+author='David Romero',
+author_email='dromero@dromero.dev',
+description='A python client for the Schneider Electric application IT Advisor.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
