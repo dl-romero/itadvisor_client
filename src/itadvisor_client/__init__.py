5855f8d71b63cf6e38b9d24f8944112e3d2d0dc7
David Romero <dromero@qualcomm.com>
2024-09-13 16:01:39 -0700

    update

--- src/itadvisor_client/__init__.py
+++ src/itadvisor_client/__init__.py
@@ -29,9 +29,38 @@ from .itadvisor_client import(ITAdvisor,
         _UserGroups,
         _UserMessage,
         _Users,
-         _WorkOrders)
+        _WorkOrders)
 
-__all__ = ('ITAdvisor')
+__all__ = (ITAdvisor,
+        _Assets,
+        _AuditTrail,
+        _Authentication,
+        _AuthenticationServers,
+        _Backup,
+        _Certificates,
+        _ChangeRequest,
+        _ChangeRequestTemplate,
+        _Configuration,
+        _CustomProperties,
+        _Customers,
+        _CustomersCount,
+        _EquipmentBrowser,
+        _ETLConfiguration,
+        _Genomes,
+        _KPIS,
+        _Licenses,
+        _Mail,
+        _PlatformStatus,
+        _PowerCapacity,
+        _PowerPath,
+        _Routing,
+        _SensorMapping,
+        _StruxureOn,
+        _SVG,
+        _UserGroups,
+        _UserMessage,
+        _Users,
+        _WorkOrders)
 
 if __name__ == "__main__":
     pass
